21165c9ceedf5965dd0345642833522fbfdc49a8
Jone Bulande <Joneluis.jl@gmail.com>
2020-05-01 22:05:07 +0300

    adicionei os arquivos do joginho de matematica em javascript

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function yeah(argument) {
+	
+	var n1 = Number(document.getElementById('n1').innerHTML);
+	var n2 = Number(document.getElementById('n2').innerHTML);
+	var n3 = document.getElementById('n3').value;
+
+	if((n1+n2) == n3){
+		alert("ACERTOU!");
+	}else{
+		alert("ERROU!");
+	}
+	resetar();
+}
+function resetar() {
+	alt1 = Math.floor((Math.random() * 10));
+	alt2 = Math.floor((Math.random() * 12));
+	document.getElementById("n3").value = "";
+	document.getElementById("n1").innerHTML = alt1;
+	document.getElementById("n2").innerHTML = alt2;
+}
