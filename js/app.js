b36929bf44827c4b698edf76f35e8cb88ff0b533
Jone <40224762+JoneBulande@users.noreply.github.com>
2020-05-02 17:01:26 +0300

    Melhorando a lógica
    Melhorando a Lógica do jogo

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 function yeah(argument) {
-	
-	var n1 = Number(document.getElementById('n1').innerHTML);
-	var n2 = Number(document.getElementById('n2').innerHTML);
 	var n3 = document.getElementById('n3').value;
-
-	if((n1+n2) == n3){
-		alert("ACERTOU!");
-	}else{
-		alert("ERROU!");
+	if (n3 != "") {
+		var n1 = Number(document.getElementById('n1').innerHTML);
+		var n2 = Number(document.getElementById('n2').innerHTML);
+		if((n1+n2) == n3){
+			alert("ACERTOU!");
+		}else{
+			alert("ERROU!");
+		}
+		resetar();
 	}
-	resetar();
-}
+}	
 function resetar() {
 	alt1 = Math.floor((Math.random() * 10));
 	alt2 = Math.floor((Math.random() * 12));
