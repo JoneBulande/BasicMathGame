3dccaf161e93e42f3b24991e9c4dbbd16ed4d381
jone <40224762+JoneBulande@users.noreply.github.com>
2022-08-20 01:43:54 +0200

    Revert "Tela de Divisão"

--- js/app.js
+++ js/app.js
@@ -24,17 +24,10 @@ function resetar() {
 	document.getElementById("n1").innerHTML = alt1;
 	document.getElementById("n2").innerHTML = alt2;
 }
-function resetardivisao() {
-	alt1 = Math.floor((Math.random() * 12))+1;
-	alt2 = Math.floor((Math.random() * 10 ))+1;
-	document.getElementById("n3").value = "";
-	document.getElementById("n1").innerHTML = alt1*alt2;
-	document.getElementById("n2").innerHTML = alt2;
-}
 /********************************************************************/
 /********************************| subtração |**********************************/
 /********************************************************************/
-function sub() {
+function sub(argument) {
 	var n3 = document.getElementById('n3').value;
 	if (n3 != "") {
 		var n1 = Number(document.getElementById('n1').innerHTML);
@@ -53,7 +46,7 @@ function sub() {
 /********************************************************************/
 /********************************| multiplicação |**********************************/
 /********************************************************************/
-function mult() {
+function mult(argument) {
 	var n3 = document.getElementById('n3').value;
 	if (n3 != "") {
 		var n1 = Number(document.getElementById('n1').innerHTML);
@@ -67,24 +60,5 @@ function mult() {
 	}else{
 		alert("Preencha o campo")
 		resetar();
-	}
-}	
-/********************************************************************/
-/********************************| divisão |**********************************/
-/********************************************************************/
-function divisao() {
-	var n3 = document.getElementById('n3').value;
-	if (n3 != "") {
-		var n1 = Number(document.getElementById('n1').innerHTML);
-		var n2 = Number(document.getElementById('n2').innerHTML);
-		if((n1/n2) == n3){
-			alert("ACERTOU!");
-		}else{
-			alert("ERROU!");
-		}
-		resetardivisao();
-	}else{
-		alert("Preencha o campo")
-		resetardivisao();
 	}
 }	
